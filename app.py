4ba826935d5c73b14e9b05817ee47517d0d2533b
Gauri kumari <khushijaaiswal0960965@gmail.com>
2026-06-04 12:30:52 +0530

    Update Streamlit function calls to use data method

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import streamlit as st
 import pandas as pd
 data = pd.read_csv("D:\Applied AI\carprice.csv")
-st.set_page_config(page_title="car price prediction App")
+st.set_page_config.data(page_title="car price prediction App")
 
-st.title("car price prediction dashboard")
+st.title.data("car price prediction dashboard")
 
-mileage = st.text_input("Enter mileage")
-fuel_type = st.text_input("Enter fuel type")
+mileage = st.text_input.data("Enter mileage")
+fuel_type = st.text_input.data("Enter fuel type")
 
-year = st.selectbox(
-    "Select year".
-    data
+year = st.selectbox.data(
+    "Select year"
 )
 
-if st.button("Submit"):
-    st.success("Successful")
-    st.write("mileage:", mileage)
-    st.write("fuel_type:", fuel_type)
-    st.write("year:", year)
+if st.button.data("Submit"):
+    st.success.data("Successful")
+    st.write.data("mileage:", mileage)
+    st.write.data("fuel_type:", fuel_type)
+    st.write.data("year:", year)
