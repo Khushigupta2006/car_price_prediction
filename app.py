e6ecab89ab18e7c33c26a982f7d97437970110c0
Gauri kumari <khushijaaiswal0960965@gmail.com>
2026-06-04 12:34:19 +0530

    Fix Streamlit method calls in app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import pandas as pd
 data = pd.read_csv("D:\Applied AI\carprice.csv")
-st.set_page_config.data(page_title="car price prediction App")
+data.st.set_page_config(page_title="car price prediction App")
 
-st.title.data("car price prediction dashboard")
+data.st.title("car price prediction dashboard")
 
-mileage = st.text_input.data("Enter mileage")
-fuel_type = st.text_input.data("Enter fuel type")
+mileage = data.st.text_input("Enter mileage")
+fuel_type = data.st.text_input("Enter fuel type")
 
-year = st.selectbox.data(
+year = data.st.selectbox(
     "Select year"
 )
 
-if st.button.data("Submit"):
-    st.success.data("Successful")
-    st.write.data("mileage:", mileage)
-    st.write.data("fuel_type:", fuel_type)
-    st.write.data("year:", year)
+if data.st.button("Submit"):
+    data.st.success.data("Successful")
+    data.st.write.data("mileage:", mileage)
+    data.st.write.data("fuel_type:", fuel_type)
+    data.st.write.data("year:", year)
