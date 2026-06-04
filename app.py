2a1ed6b3403ba5297b68c753b9d4a8ce1d561e25
Gauri kumari <khushijaaiswal0960965@gmail.com>
2026-06-04 12:04:13 +0530

    Create car price prediction dashboard with Streamlit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+data = pd.read_csv("D:\Applied AI\carprice.csv")
+st.set_page_config(page_title="car price prediction App")
+
+st.title("car price prediction dashboard")
+
+mileage = st.text_input("Enter mileage")
+fuel type = st.number_input("Enter fuel type")
+
+year = st.selectbox(
+    "Select year",
+    [data]
+)
+
+if st.button("Submit"):
+    st.success("Successful")
+    st.write("mileage:", mileage)
+    st.write("fuel type:", fuel type)
+    st.write("year:", year)
